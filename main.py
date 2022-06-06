15a342a2f32070c7988cb6d34ee612bf583d9a20
Karin van Homelen <karinvanhomelen@live.nl>
2022-06-06 19:02:28 +0200

    setting up credentials

---
+++ main.py
@@ -0,0 +1,10 @@
+import config
+import tweepy
+
+client_id = config.CLIENT_ID
+client_secret = config.CLIENT_SECRET
+bearer_token = config.BEARER_TOKEN
+consumer_key = config.CONSUMER_KEY
+consumer_secret = config.CONSUMER_SECRET
+access_token = config.ACCESS_TOKEN
+access_token_secret = config.ACCESS_TOKEN_SECRET
